0e838e82afddfbf469c4601301bb9217b398b333
Marshall Nickolauson <marshnickol101@gmail.com>
2025-03-07 05:37:40 -0500

    add: fonts and colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        
-      },
-      fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-        ropa: ['Ropa Sans', 'sans-serif'],
-      },
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                mainGreen: "#2C9F46",
+                mainBlack: "#2C2E35",
+                mainWhite: "#FFFFFF",
+
+                lightGreen: "#52BC4A",
+                darkGreen: "#236831",
+                creamExpansion: "#E6F3DB",
+                darkExpansion: "#201D1F",
+                mainBlue: "#2256AD",
+                mainRed: "#CD281A",
+                mainOrange: "#FF611F",
+                mainTan: "#FBEFCB",
+                mainYellow: "#FFD639",
+            },
+            fontFamily: {
+                roboto: ["Roboto", "sans-serif"],
+                ropa: ["Ropa Sans", "sans-serif"],
+                poppins: ["Poppins", "sans-serif"],
+                opensans: ["Open Sans", "sans-serif"],
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
