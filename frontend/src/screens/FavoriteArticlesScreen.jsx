5ee3f3969847fcff0335870cfae4c66977f4246b
marshall nickolauson <marshnickol101@gmail.com>
2025-03-20 10:33:29 -0400

    feature: favorites screen date sorting

--- frontend/src/screens/FavoriteArticlesScreen.jsx
+++ frontend/src/screens/FavoriteArticlesScreen.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ArticleListCard from '../components/ArticleListCard';
 import { useGetAllArticlesQuery } from '../slices/article/articleApiSlice';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import RecentCardLoader from '../components/loaders/RecentCardLoader';
 import { TRANSLATIONS } from '../constants';
+import { FaRegArrowAltCircleDown } from 'react-icons/fa';
 
 const FavoriteArticlesScreen = () => {
     const navigate = useNavigate();
@@ -21,13 +22,20 @@ const FavoriteArticlesScreen = () => {
         refetch();
     }, [refetch]);
 
-    // Filter articles to show only favorited ones
     const favoriteArticles = articles.filter((article) => userInfo?.favorites?.some((fav) => fav.articleId === article._id));
+
+    const [isRotated, setIsRotated] = useState(false);
+
+    const sortedArticles = isRotated
+        ? [...favoriteArticles].sort((a, b) => new Date(a.languages[language]?.date) - new Date(b.languages[language]?.date)) // ASC by date
+        : [...favoriteArticles].sort((a, b) => new Date(b.languages[language]?.date) - new Date(a.languages[language]?.date)); // DESC by date
 
     if (isLoading || isFetching) {
         return (
             <div className='flex flex-col'>
-                <h1 className={`py-5 font-poppins font-bold text-4xl transition-all duration-200 ${isDarkMode ? 'text-white' : 'text-darkExpansion'}`}>{translations.favoriteArticles || 'Favorite Articles'}</h1>
+                <h1 className={`py-5 font-poppins font-bold text-4xl transition-all duration-200 ${isDarkMode ? 'text-white' : 'text-darkExpansion'}`}>
+                    {translations.favoriteArticles || 'Favorite Articles'}
+                </h1>
                 <div className='h-[460px] pt-1'>
                     {[...Array(8)].map((_, index) => (
                         <div className='pb-5' key={index}>
@@ -55,14 +63,21 @@ const FavoriteArticlesScreen = () => {
 
     return (
         <div>
-            <div className='flex flex-col'>
+            <div className='flex items-center space-x-4'>
                 <h1 className={`py-5 font-poppins font-bold text-4xl transition-all duration-200 ${isDarkMode ? 'text-white' : 'text-darkExpansion'}`}>
                     {translations.favoriteArticles || 'Favorite Articles'}
                 </h1>
+                <div className='my-4 flex space-x-3 items-center'>
+                    <FaRegArrowAltCircleDown
+                        size={35}
+                        onClick={() => setIsRotated(!isRotated)}
+                        className={`transition-transform duration-500 cursor-pointer transform ${isRotated ? 'rotate-180' : ''}`}
+                    />
+                </div>
             </div>
             <div className='space-y-4 py-2'>
                 {favoriteArticles.length > 0 ? (
-                    favoriteArticles.map((article) => <ArticleListCard key={article._id} article={article} />)
+                    sortedArticles.map((article) => <ArticleListCard key={article._id} article={article} />)
                 ) : (
                     <p className={`text-center text-lg ${isDarkMode ? 'text-white' : 'text-gray-600'}`}>{translations.noFavorites || 'No favorite articles yet.'}</p>
                 )}
