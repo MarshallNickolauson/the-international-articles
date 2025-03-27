9e2c8e0e0a2d6289a55f57ab8d274c62543f8000
marshall nickolauson <marshnickol101@gmail.com>
2025-03-26 22:53:19 -0400

    update: dark mode

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
@@ -7,11 +7,15 @@ import { useSelector } from 'react-redux';
 const MainLayout = () => {
     const isDarkMode = useSelector((state) => state.theme.isDarkMode);
 
+    useEffect(() => {
+        document.body.style.backgroundColor = isDarkMode ? '#252825' : '#ffffff';
+    }, [isDarkMode]);
+
     return (
-        <div className={`${isDarkMode ? 'bg-_252825' : 'bg-white'} transition-all duration-200 pb-[1000px]`}>
+        <div className={`${isDarkMode ? 'bg-_252825' : 'bg-white'} transition-all duration-200`}>
             <Header />
             <div className='px-4'>
-                <div className='max-w-[1450px] mx-auto '>
+                <div className='max-w-[1450px] mx-auto pb-[500px]'>
                     <Outlet />
                 </div>
             </div>
