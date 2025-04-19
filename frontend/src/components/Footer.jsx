a009c7bac1372742ad57f015bdc485c4ef2b2f26
Marshall Nickolauson <marshnickol101@gmail.com>
2025-04-19 14:37:46 -0400

    add: footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,14 @@
+import { useSelector } from "react-redux";
+
 function Footer() {
+    const isDarkMode = useSelector((state) => state.theme.isDarkMode);
+
+    const year = new Date().getFullYear();
+
     return (
-        <footer className='px-4'>
-            <div className='max-w-[1450px] mx-auto'>
-                
+        <footer className="px-4">
+            <div className="max-w-[1450px] mx-auto">
+                <p className={`text-md text-center ${isDarkMode ? 'text-white' : 'text-darkExpansion'}`}>Copyright Example &copy; The International Articles {year}</p>
             </div>
         </footer>
     );
